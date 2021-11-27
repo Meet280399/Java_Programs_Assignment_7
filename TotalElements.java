4a74abb8f7c982775066de8d9e260cd0d10f4209
Meet280399 <74697242+Meet280399@users.noreply.github.com>
2021-11-27 19:58:05 +0530

    Add files via upload

---
+++ TotalElements.java
@@ -0,0 +1,13 @@
+package Assignment_7;
+
+import java.util.*;
+import java.util.stream.*;
+public class TotalElements {
+    public static void main(String args[]) {
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+        long count =  myList.stream()
+                .count();
+        System.out.println(count);
+    }
+}
+
